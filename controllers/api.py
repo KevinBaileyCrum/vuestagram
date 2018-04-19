cac11cb2619e626b570bf89b5afc0d4cb8c466e1
Luca de Alfaro <luca@dealfaro.org>
2018-04-19 14:57:01 -0700

    We can get fake tracks.

--- controllers/api.py
+++ controllers/api.py
@@ -1,1 +1,25 @@
 # Here go your api methods.
+
+import random
+
+# Mocks implementation.
+def get_tracks():
+    start_idx = int(request.vars.start_idx) if request.vars.start_idx is not None else 0
+    end_idx = int(request.vars.end_idx) if request.vars.end_idx is not None else 0
+    # We just generate a lot of of data.
+    tracks = []
+    for i in range(start_idx, end_idx):
+        t = dict(
+            artist = random.choice(['IU', 'Ailee', 'T-ara', 'Mamamoo']),
+            album = random.choice(['Modern times', 'Melting', 'Absolute']),
+            title = random.choice(['Falling U', 'TTL', 'Piano Man']),
+            num_plays = random.randint(0, 100),
+        )
+        tracks.append(t)
+    has_more = True
+    logged_in = False
+    return response.json(dict(
+        tracks=tracks,
+        logged_in=logged_in,
+        has_more=has_more,
+    ))
--- static/js/default_index.js
+++ static/js/default_index.js
@@ -13,18 +13,47 @@ var app = function() {
         }
     };
 
-    // Complete as needed.
+    function get_tracks_url(start_idx, end_idx) {
+        var pp = {
+            start_idx: start_idx,
+            end_idx: end_idx
+        };
+        return "/start/api/get_tracks" + "?" + $.param(pp);
+    }
+
+    self.get_tracks = function () {
+        $.getJSON(get_tracks_url(0, 10), function (data) {
+            self.vue.tracks = data.tracks;
+            self.vue.has_more = data.has_more;
+            self.vue.logged_in = data.logged_in;
+        })
+    };
+
+    self.get_more = function () {
+        var num_tracks = self.vue.tracks.length;
+        $.getJSON(get_tracks_url(num_tracks, num_tracks + 10), function (data) {
+            self.vue.has_more = data.has_more;
+            self.extend(self.vue.tracks, data.tracks);
+        });
+    };
+
     self.vue = new Vue({
         el: "#vue-div",
         delimiters: ['${', '}'],
         unsafeDelimiters: ['!{', '}'],
         data: {
+            tracks: [],
+            logged_in: false,
+            has_more: false
         },
         methods: {
+            get_more: self.get_more
         }
 
     });
 
+    self.get_tracks();
+    $("#vue-div").show();
 
     return self;
 };

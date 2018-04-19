a68ebe307b48645b2c239a6f44bbcd4fd90d58ec
Luca de Alfaro <luca@dealfaro.org>
2018-04-19 15:03:21 -0700

    Prototype for a better API

--- controllers/api.py
+++ controllers/api.py
@@ -1,25 +1,46 @@
 # Here go your api methods.
 
-import random
+# Let us have a serious implementation now.
 
-# Mocks implementation.
 def get_tracks():
     start_idx = int(request.vars.start_idx) if request.vars.start_idx is not None else 0
     end_idx = int(request.vars.end_idx) if request.vars.end_idx is not None else 0
-    # We just generate a lot of of data.
     tracks = []
-    for i in range(start_idx, end_idx):
-        t = dict(
-            artist = random.choice(['IU', 'Ailee', 'T-ara', 'Mamamoo']),
-            album = random.choice(['Modern times', 'Melting', 'Absolute']),
-            title = random.choice(['Falling U', 'TTL', 'Piano Man']),
-            num_plays = random.randint(0, 100),
-        )
-        tracks.append(t)
-    has_more = True
-    logged_in = False
+    has_more = False
+    rows = db().select(db.track.ALL, limitby=(start_idx, end_idx + 1))
+    for i, r in enumerate(rows):
+        if i < end_idx - start_idx:
+            t = dict(
+                id = r.id,
+                artist = r.artist,
+                album = r.album,
+                title = r.title,
+                duration = r.duration,
+                rating = r.rating,
+                num_plays = r.num_plays,
+            )
+            tracks.append(t)
+        else:
+            has_more = True
+    logged_in = auth.user is not None
     return response.json(dict(
         tracks=tracks,
         logged_in=logged_in,
         has_more=has_more,
     ))
+
+@auth.requires_signature()
+def add_track():
+    t_id = db.track.insert(
+        artist = request.vars.artist,
+        album = request.vars.album,
+        title = request.vars.title,
+        num_plays = 0
+    )
+    t = db.track(t_id)
+    return response.json(dict(track=t))
+
+@auth.requires_signature()
+def del_track():
+    db(db.track.id == request.vars.track_id).delete()
+    return "ok"

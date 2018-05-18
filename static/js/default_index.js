815bdca1167d5e44157c548b93b2ecab9a03b78c
Luca de Alfaro <luca@dealfaro.org>
2018-05-17 17:34:37 -0700

    Initial commit

--- static/js/default_index.js
+++ static/js/default_index.js
@@ -16,117 +16,14 @@ var app = function() {
     // Enumerates an array.
     var enumerate = function(v) { var k=0; return v.map(function(e) {e._idx = k++;});};
 
-    self.insertion_id = null; // Initialization.
-
-    function get_tracks_url(start_idx, end_idx) {
-        var pp = {
-            start_idx: start_idx,
-            end_idx: end_idx
-        };
-        return tracks_url + "&" + $.param(pp);
-    }
-
-    self.get_tracks = function () {
-        $.getJSON(get_tracks_url(0, 10), function (data) {
-            self.vue.tracks = data.tracks;
-            self.vue.has_more = data.has_more;
-            self.vue.logged_in = data.logged_in;
-            enumerate(self.vue.tracks);
-        })
-    };
-
-    self.get_more = function () {
-        var num_tracks = self.vue.tracks.length;
-        $.getJSON(get_tracks_url(num_tracks, num_tracks + 10), function (data) {
-            self.vue.has_more = data.has_more;
-            self.extend(self.vue.tracks, data.tracks);
-            enumerate(self.vue.tracks);
-        });
-    };
-
-    self.add_track_button = function () {
-        // This button is used to start the add of a new track; begins with upload.
-        // Show the Dropzone plugin.
-        $("div#uploader_div").show();
-        // The button to add a track has been pressed.
-        self.vue.is_adding_track = true;
-    };
-
-    self.upload_complete = function (response) {
-        // This callback is called when the insertion of the track completes.
-        // The next step will be to get the track info (artist, album, etc).
-        // Notes the insertion id.
-        self.insertion_id = response.insertion_id;
-        // Moves to entering the track info: displays the form, rather than the uploader.
-        self.vue.is_adding_track_info = true;
-        self.vue.is_adding_track = false;
-        $("div#uploader_div").hide();
-    };
-
-    self.add_track = function () {
-        // Submits the track info.
-        // This is the last step of the track insertion process.
-        $.post(add_track_url,
-            {
-                artist: self.vue.form_artist,
-                title: self.vue.form_track,
-                album: self.vue.form_album,
-                insertion_id: self.insertion_id
-            },
+    self.get_upload_url = function () {
+        $.get_json('https://upload-dot-luca-teaching.appspot.com/start/uploader/get_upload_url',
             function (data) {
-                $.web2py.enableElement($("#add_track_submit"));
-                self.vue.is_adding_track_info = false;
-                self.vue.is_adding_track = false;
-                self.vue.tracks.unshift(data.track);
-                enumerate(self.vue.tracks);
-                self.vue.form_artist = "";
-                self.vue.form_track = "";
-                self.vue.form_album = "";
-            });
-    };
+                self.put_url = data['signed_url'];
+                self.get_url = data['access_url'];
 
-    self.delete_track = function(track_idx) {
-        $.post(del_track_url,
-            { track_id: self.vue.tracks[track_idx].id },
-            function () {
-                self.vue.tracks.splice(track_idx, 1);
-                enumerate(self.vue.tracks);
             }
-        )
-    };
-
-    self.cancel_add_track = function() {
-        self.vue.is_adding_track_info = false;
-        self.vue.is_adding_track = false;
-        $("div#uploader_div").hide();
-        $.post(cleanup_url); // Cleans up any incomplete uploads.
-    };
-
-    self.select_track = function(track_idx) {
-        var track = self.vue.tracks[track_idx];
-        if (self.vue.selected_id === track.id) {
-            // Deselect track.
-            self.vue.selected_id = -1;
-        } else {
-            // Select it.
-            self.vue.selected_idx = track_idx;
-            self.vue.selected_id = track.id;
-            self.vue.selected_url = track.track_url;
-        }
-        if (self.vue.selected_url && self.vue.selected_id > -1) {
-            // We play the track.
-            self.inc_play_track(track_idx);
-        }
-    };
-
-    self.inc_play_track = function (track_idx) {
-        var track = self.vue.tracks[track_idx];
-        track.num_plays += 1;
-        $.post(
-            inc_plays_url,
-            {track_id: track.id},
-            function () {}
-        )
+            )
     };
 
 

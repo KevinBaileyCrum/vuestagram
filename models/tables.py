97e5edfa3576df812dd575c815760efb9cd292ee
Kevin Crum <kevinbaileycrum@gmail.com>
2018-06-04 02:04:13 -0700

    is_checked added to db

--- models/tables.py
+++ models/tables.py
@@ -55,5 +55,6 @@ db.define_table('images',
                 Field('created_by', 'reference auth_user', default=auth.user_id),
                 Field('image_url'),
                 Field('image_price','integer'),
+                Field('is_checked', 'boolean', default=False),
 )
 
